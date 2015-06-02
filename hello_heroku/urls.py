3be50e1c2056f99f018b696ade4e6ca052af8d23
David Gouldin <david@gould.in>
2015-06-02 12:43:29 +0100

    add logging

--- hello_heroku/urls.py
+++ hello_heroku/urls.py
@@ -1,7 +1,10 @@
+import logging
 from django.conf.urls import url
 from django.http import HttpResponse
+logging.basicConfig(level=logging.DEBUG)
 
 def hello_world(request):
+    logging.info('saying hello')
     return HttpResponse('Hello World!')
 
 urlpatterns = [url(r'^$', hello_world)]
