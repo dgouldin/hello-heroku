caaa8bcd4ee5ad269eafccba769830ff5b7a5f59
David Gouldin <david@gould.in>
2015-06-02 12:47:14 +0100

    use NAME env var

--- hello_heroku/urls.py
+++ hello_heroku/urls.py
@@ -1,10 +1,12 @@
 import logging
+import os
 from django.conf.urls import url
 from django.http import HttpResponse
 logging.basicConfig(level=logging.DEBUG)
 
 def hello_world(request):
     logging.info('saying hello')
-    return HttpResponse('Hello World!')
+    name = os.environ.get('NAME', 'World')
+    return HttpResponse('Hello {}!'.format(name))
 
 urlpatterns = [url(r'^$', hello_world)]
